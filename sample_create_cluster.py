2f0a308684ade9afa72c9bd5fdac39b2997839dd
pietro-nobili-SDG <pietro.nobili@sdggroup.com>
2022-11-09 16:33:26 +0100

    Add more info in create_cluster

--- sample_create_cluster.py
+++ sample_create_cluster.py
@@ -4,11 +4,13 @@ https://docs.databricks.com/dev-tools/python-api.html
 """
 
 import json
+from typing import Dict
 
 from databricks_cli.clusters.api import ClusterApi
 from loguru import logger as lg
+from requests import Response
 
-from utils import get_databricks_client
+from utils import get_databricks_client, jd
 
 CREATE_TEMPLATE = """{{
     "num_workers": 0,
@@ -39,16 +41,29 @@ CREATE_TEMPLATE = """{{
 
 def sample_create_cluster():
     """Sample use of the create_cluster func."""
+    # get the api handler
     api_client = get_databricks_client()
     cluster_api = ClusterApi(api_client)
+
+    # build the string and get a dict from it
     json_conf_str = CREATE_TEMPLATE.format(
         cluster_name="test_databricks_api_01",
         autotermination_minutes=10,
     )
     lg.info("Will create cluster with conf string:\n{}", json_conf_str)
     json_conf = json.loads(json_conf_str)
+
+    # add a debug idempotency token so we can spam this request
+    json_conf.update({"idempotency_token": "testing_testing_123"})
     lg.info("Will create cluster with conf:\n{}", json_conf)
-    cluster_api.create_cluster(json_conf)
+
+    # create the cluster
+    create_response: Dict = cluster_api.create_cluster(json_conf)
+
+    # get the id
+    lg.debug(f"{type(create_response)=}")
+    lg.debug(f"{jd(create_response)=}")
+    lg.debug(f"{create_response['cluster_id']=}")
 
 
 if __name__ == "__main__":
